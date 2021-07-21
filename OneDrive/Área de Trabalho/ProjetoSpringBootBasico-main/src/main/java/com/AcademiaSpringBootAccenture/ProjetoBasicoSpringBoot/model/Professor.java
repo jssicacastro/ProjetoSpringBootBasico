d5cbe9bd8a67af3e1732889f94bde370017da376
giovannigomes123 <70283937+giovannigomes123@users.noreply.github.com>
2021-07-20 22:12:29 -0300

    Update Professor.java

--- OneDrive/Área de Trabalho/ProjetoSpringBootBasico-main/src/main/java/com/AcademiaSpringBootAccenture/ProjetoBasicoSpringBoot/model/Professor.java
+++ OneDrive/Área de Trabalho/ProjetoSpringBootBasico-main/src/main/java/com/AcademiaSpringBootAccenture/ProjetoBasicoSpringBoot/model/Professor.java
@@ -5,6 +5,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.GroupSequence;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CPF;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,13 +26,14 @@ import lombok.ToString;
 @ToString
 @Entity
 @Table
+@GroupSequence(Professor.class)
 public class Professor {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	private int idprofessor;
-	private String nome;
-	private String cpf;
-	private int idade;
-	private double salario;
+	private int idprofessor;		
+	@NotBlank(message = "o nome é obrigatório") @Length(min = 3, max = 30, message = "O nome deverá ter no máximo {max} caracteres")private String nome;
+	@CPF(message="O CPF deve ser válido") private String cpf;
+	@Positive(message="A idade deve ser válida") private int idade;
+	@PositiveOrZero(message="O salário deve ser válido") private double salario;
 }
